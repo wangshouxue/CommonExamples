188e52f707ccaa495aabeef1d9cf63bf76761394
wsx <1556090086@qq.com>
2022-04-09 15:12:03 +0800

    桌面APP图标长按快捷方式入口

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # CommonExamples
 常用小示例
+
+baseviewbinging viewbinging的基本使用
+mvpdemo  mvp小示例
+privacydialogdemo 隐私协议组件
+shortcutsdemo 桌面APP图标长按快捷方式入口
--- shortcutsdemo/src/main/java/com/wsx/shortcutsdemo/MainActivity.kt
+++ shortcutsdemo/src/main/java/com/wsx/shortcutsdemo/MainActivity.kt
@@ -10,10 +10,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        方式1 在AndroidManifest.xml中
+
+//        方式2
+//        shortcut()
+
+//        方式3
         initShortcuts()
         jumpShortcuts()
     }
 
+    fun shortcut(){
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N_MR1) {
+            val scManager: ShortcutManager = getSystemService(ShortcutManager::class.java)
+            //跳转Second
+            val secondIntent = Intent("secondback",android.net.Uri.EMPTY,
+                this,Test::class.java) //此处的Test即为回退页面，快捷方式跳转的页面按下返回键后显示的页面
+            val infoPublishPai: ShortcutInfo =ShortcutInfo.Builder(this, "two")
+                .setShortLabel("go second")
+                .setRank(1)
+                .setIcon(android.graphics.drawable.Icon.createWithResource(this,R.drawable.icon_shortcut_pai))
+                .setIntents(arrayOf<Intent>(secondIntent,Intent(this,Second::class.java).setAction("second")))
+                .build()
+            val list: MutableList<ShortcutInfo> = java.util.ArrayList<ShortcutInfo>()
+            list.add(infoPublishPai)
+
+            if (scManager != null) {
+                scManager.setDynamicShortcuts(list)
+            }
+        }
+    }
     /**
      * 7.1以上支持shortcuts
      */
@@ -21,20 +47,23 @@ class MainActivity : AppCompatActivity() {
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N_MR1) {
             val scManager: ShortcutManager = getSystemService(ShortcutManager::class.java)
             //跳转Test
-            val testIntent = Intent("test",android.net.Uri.EMPTY,
-                this,MainActivity::class.java)
-            testIntent.putExtra("shortcuts", "test")
-            val infoForumPublish: ShortcutInfo = ShortcutInfo.Builder(this, "test")
+            val testIntent = Intent(Intent.ACTION_MAIN,android.net.Uri.EMPTY,
+                this,MainActivity::class.java)//此处需为入口页面，否则快捷方式目标页面跳转不了
+            testIntent.putExtra("shortcuts", "test")//自定义的规则，解析跳转时用到
+            val infoForumPublish: ShortcutInfo = ShortcutInfo.Builder(this, "testid")
                 .setShortLabel("测试一下")
+                .setRank(0)
                 .setIcon(android.graphics.drawable.Icon.createWithResource(this, R.drawable.icon_shortcut_post))
                 .setIntents(arrayOf<Intent>(testIntent))
                 .build()
+//            action ,id 可以随便填
             //跳转Second
-            val secondIntent = Intent("second",android.net.Uri.EMPTY,
+            val secondIntent = Intent("secondaction",android.net.Uri.EMPTY,
                 this,MainActivity::class.java)
             secondIntent.putExtra("shortcuts", "second")
-            val infoPublishPai: ShortcutInfo =ShortcutInfo.Builder(this, "second")
+            val infoPublishPai: ShortcutInfo =ShortcutInfo.Builder(this, "secondid")
                 .setShortLabel("go second")
+                .setRank(1)
                 .setIcon(android.graphics.drawable.Icon.createWithResource(this,R.drawable.icon_shortcut_pai))
                 .setIntents(arrayOf<Intent>(secondIntent))
                 .build()
